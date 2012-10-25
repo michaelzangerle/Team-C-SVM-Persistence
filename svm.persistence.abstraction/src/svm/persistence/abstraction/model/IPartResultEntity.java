7dc162d6d498f7100bb3c3ad478c27e5d2c678a2
Michael Zangerle <michael.zangerle@gmail.com>
2012-10-25 10:21:45 +0200

    Michael DAOs and Mappings incl. Interfaces (1)

--- svm.persistence.abstraction/src/svm/persistence/abstraction/model/IPartResultEntity.java
+++ svm.persistence.abstraction/src/svm/persistence/abstraction/model/IPartResultEntity.java
@@ -1,5 +1,7 @@
 package svm.persistence.abstraction.model;
 
+import svm.persistence.hibernate.model.ContestantEntity;
+
 /**
  * ProjectTeam: Team C
  * Date: 25.10.12
@@ -15,4 +17,10 @@ public interface IPartResultEntity extends IEntity {
     String getComment();
 
     void setComment(String comment);
+
+    @ManyToOne(cascade = CascadeType.DETACH, targetEntity = ContestantEntity.class, fetch = FetchType.LAZY)
+    @JoinColumn(name = "contestant")
+    ContestantEntity getContestant();
+
+    void setContestant(ContestantEntity contestant);
 }
