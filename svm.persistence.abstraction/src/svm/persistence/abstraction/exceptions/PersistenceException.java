20895d12443f4a536277a936acea20c25fdfe66c
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-03 16:27:26 +0100

    Remote Interface hinzugefügt

--- svm.persistence.abstraction/src/svm/persistence/abstraction/exceptions/PersistenceException.java
+++ svm.persistence.abstraction/src/svm/persistence/abstraction/exceptions/PersistenceException.java
@@ -1,9 +1,12 @@
 package svm.persistence.abstraction.exceptions;
 
+import java.io.Serializable;
+import java.rmi.Remote;
+
 /**
  * Projectteam: Team C
  * Date: 19.10.12
  * Marks a Exception in Persistence Layer
  */
-public class PersistenceException extends Exception {
+public class PersistenceException extends Exception implements Remote,Serializable {
 }
