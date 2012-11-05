fb8953b2c8c312463ab4d2d31142116ca980fea0
thomasgaida <thomas.gaida@students.fhv.at>
2012-11-05 09:56:10 +0100

    Remote Interface hinzugefügt

--- svm.persistence.abstraction/src/svm/persistence/abstraction/exceptions/NoTransactionException.java
+++
@@ -1,9 +0,0 @@
-package svm.persistence.abstraction.exceptions;
-
-/**
- * Projectteam: Team C
- * Date: 19.10.12
- * Marks if no Transaction opened
- */
-public class NoTransactionException extends PersistenceException {
-}
